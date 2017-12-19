e639d39fcbd39e01956fb7e84682c50143f2c588
Pooja Kodavanti <pkodavan@gmail.com>
2017-12-19 21:48:45 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,56 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  var array = [1]
+  var element = 'foo'
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  var array = [1]
+  var element = 'foo'
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  var array = [1]
+  var element = 'foo'
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  var array = [1]
+  var element = 'foo'
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index) {
+  var array = [1, 2, 3]
+  return array[2]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  var array = [1, 2, 3]
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  var array = [1, 2, 3]
+  array = array.slice(1)
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  var array = [1, 2, 3]
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  var array = [1, 2, 3]
+  array = array.slice(0, length - 1)
+  return array
+}
